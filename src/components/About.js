6d56aa99f7eaffdceb1bc64ee1c4762eda919eaf
NgunyiGachie <antogachie@gmail.com>
2024-04-24 14:13:43 +0300

    Complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Links from "./Links"; 
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+function About({ user }) {
+    const { bio, links } = user || {};
+    const { github, linkedin } = links || {};
+
+    return (
+        <div id="about">
+            <h2>About Me {user?.name}</h2>
+            {bio && <p>{bio}</p>}
+            <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+            {github && linkedin && <Links github={github} linkedin={linkedin} />} 
+        </div>
+    );
 }
 
 export default About;
+
--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,17 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
+// pass this data down as props to the child component(s) that need it
 import user from "../data/user";
 console.log(user);
 
 function App() {
+
   return (
     <div>
-      <NavBar />
-      <Home />
-      <About />
+      <NavBar/>
+      <Home user={user} color={user.color} />
+      <About user={user} bio={user.bio}/>
     </div>
   );
 }
